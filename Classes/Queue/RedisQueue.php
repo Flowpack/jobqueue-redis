7161c82678095e54a512ed98fbeb311715d98c57
Bastian <b.waidelich@wwwision.de>
2016-06-06 11:49:58 +0200

    TASK: Adjust to latest Flowpack.JobQueue.Common changes
    * Adjust namespace from "TYPO3.Jobqueue.Redis" to "Flowpack.JobQueue.Redis"
    * Add composer manifest
    * Add MIT license
    * Fix code style according to CGL
    * Adjust to modified API (Queue::publish() -> Queue::submit())
    * Minor bugfixes

--- Classes/Queue/RedisQueue.php
+++ Classes/Queue/RedisQueue.php
@@ -1,202 +1,227 @@
 <?php
-namespace TYPO3\Jobqueue\Redis\Queue;
+namespace Flowpack\JobQueue\Redis\Queue;
 
-use TYPO3\FLOW3\Package\Package as BasePackage;
-use TYPO3\FLOW3\Annotations as FLOW3;
+/*
+ * This file is part of the Flowpack.JobQueue.Redis package.
+ *
+ * (c) Contributors to the package
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Flowpack\JobQueue\Common\Queue\Message;
+use Flowpack\JobQueue\Common\Queue\QueueInterface;
+use Predis\Client as PredisClient;
 
 /**
  * A queue implementation using Redis as the queue backend
  *
  * Depends on Predis as the PHP Redis client.
  */
-class RedisQueue implements \TYPO3\Jobqueue\Common\Queue\QueueInterface {
+class RedisQueue implements QueueInterface
+{
 
-	/**
-	 * @var string
-	 */
-	protected $name;
+    /**
+     * @var string
+     */
+    protected $name;
 
-	/**
-	 * @var \Predis\Client
-	 */
-	protected $client;
+    /**
+     * @var PredisClient
+     */
+    protected $client;
 
-	/**
-	 * @var integer
-	 */
-	protected $defaultTimeout = 60;
+    /**
+     * @var integer
+     */
+    protected $defaultTimeout = 60;
 
-	/**
-	 * Constructor
-	 *
-	 * @param string $name
-	 * @param array $options
-	 */
-	public function __construct($name, array $options = array()) {
-		$this->name = $name;
-		if (isset($options['defaultTimeout'])) {
-			$this->defaultTimeout = (integer)$options['defaultTimeout'];
-		}
-		$clientOptions = isset($options['client']) ? $options['client'] : array();
-		$this->client = new \Predis\Client($clientOptions);
-	}
+    /**
+     * Constructor
+     *
+     * @param string $name
+     * @param array $options
+     */
+    public function __construct($name, array $options = array())
+    {
+        $this->name = $name;
+        if (isset($options['defaultTimeout'])) {
+            $this->defaultTimeout = (integer)$options['defaultTimeout'];
+        }
+        $clientOptions = isset($options['client']) ? $options['client'] : array();
+        $this->client = new PredisClient($clientOptions);
+    }
 
-	/**
-	 * Publish a message to the queue
-	 *
-	 * @param \TYPO3\Jobqueue\Common\Queue\Message $message
-	 * @return void
-	 */
-	public function publish(\TYPO3\Jobqueue\Common\Queue\Message $message) {
-		if ($message->getIdentifier() !== NULL) {
-			$added = $this->client->sadd("queue:{$this->name}:ids", $message->getIdentifier());
-			if (!$added) {
-				return;
-			}
-		}
-		$encodedMessage = $this->encodeMessage($message);
-		$this->client->lpush("queue:{$this->name}:messages", $encodedMessage);
-		$message->setState(\TYPO3\Jobqueue\Common\Queue\Message::STATE_PUBLISHED);
-	}
+    /**
+     * Submit a message to the queue
+     *
+     * @param Message $message
+     * @return void
+     */
+    public function submit(Message $message)
+    {
+        if ($message->getIdentifier() !== NULL) {
+            $added = $this->client->sadd("queue:{$this->name}:ids", $message->getIdentifier());
+            if (!$added) {
+                return;
+            }
+        }
+        $encodedMessage = $this->encodeMessage($message);
+        $this->client->lpush("queue:{$this->name}:messages", $encodedMessage);
+        $message->setState(Message::STATE_SUBMITTED);
+    }
 
-	/**
-	 * Wait for a message in the queue and return the message for processing
-	 * (without safety queue)
-	 *
-	 * @param int $timeout
-	 * @return \TYPO3\Jobqueue\Common\Message The received message or NULL if a timeout occured
-	 */
-	public function waitAndTake($timeout = NULL) {
-		$timeout !== NULL ? $timeout : $this->defaultTimeout;
-		$keyAndValue = $this->client->brpop("queue:{$this->name}:messages", $timeout);
-		$value = $keyAndValue[1];
-		if (is_string($value)) {
-			$message = $this->decodeMessage($value);
+    /**
+     * Wait for a message in the queue and return the message for processing
+     * (without safety queue)
+     *
+     * @param int $timeout
+     * @return Message The received message or NULL if a timeout occurred
+     */
+    public function waitAndTake($timeout = NULL)
+    {
+        if ($timeout === NULL) {
+            $timeout = $this->defaultTimeout;
+        }
+        $keyAndValue = $this->client->brpop("queue:{$this->name}:messages", $timeout);
+        $value = $keyAndValue[1];
+        if (is_string($value)) {
+            $message = $this->decodeMessage($value);
 
-			if ($message->getIdentifier() !== NULL) {
-				$this->client->srem("queue:{$this->name}:ids", $message->getIdentifier());
-			}
+            if ($message->getIdentifier() !== NULL) {
+                $this->client->srem("queue:{$this->name}:ids", $message->getIdentifier());
+            }
 
-				// The message is marked as done
-			$message->setState(\TYPO3\Jobqueue\Common\Queue\Message::STATE_DONE);
+            // The message is marked as done
+            $message->setState(Message::STATE_DONE);
 
-			return $message;
-		} else {
-			return NULL;
-		}
-	}
+            return $message;
+        } else {
+            return NULL;
+        }
+    }
 
-	/**
-	 * Wait for a message in the queue and save the message to a safety queue
-	 *
-	 * TODO: Idea for implementing a TTR (time to run) with monitoring of safety queue. E.g.
-	 * use different queue names with encoded times? With brpoplpush we cannot modify the
-	 * queued item on transfer to the safety queue and we cannot update a timestamp to mark
-	 * the run start time in the message, so separate keys should be used for this.
-	 *
-	 * @param int $timeout
-	 * @return \TYPO3\Jobqueue\Common\Queue\Message
-	 */
-	public function waitAndReserve($timeout = NULL) {
-		$timeout !== NULL ? $timeout : $this->defaultTimeout;
-		$value = $this->client->brpoplpush("queue:{$this->name}:messages", "queue:{$this->name}:processing", $timeout);
-		if (is_string($value)) {
-			$message = $this->decodeMessage($value);
-			if ($message->getIdentifier() !== NULL) {
-				$this->client->srem("queue:{$this->name}:ids", $message->getIdentifier());
-			}
-			return $message;
-		} else {
-			return NULL;
-		}
-	}
+    /**
+     * Wait for a message in the queue and save the message to a safety queue
+     *
+     * TODO: Idea for implementing a TTR (time to run) with monitoring of safety queue. E.g.
+     * use different queue names with encoded times? With "brpoplpush" we cannot modify the
+     * queued item on transfer to the safety queue and we cannot update a timestamp to mark
+     * the run start time in the message, so separate keys should be used for this.
+     *
+     * @param int $timeout
+     * @return Message
+     */
+    public function waitAndReserve($timeout = NULL)
+    {
+        if ($timeout === NULL) {
+            $timeout = $this->defaultTimeout;
+        }
+        $value = $this->client->brpoplpush("queue:{$this->name}:messages", "queue:{$this->name}:processing", $timeout);
+        if (is_string($value)) {
+            $message = $this->decodeMessage($value);
+            if ($message->getIdentifier() !== NULL) {
+                $this->client->srem("queue:{$this->name}:ids", $message->getIdentifier());
+            }
+            return $message;
+        } else {
+            return NULL;
+        }
+    }
 
-	/**
-	 * Mark a message as finished
-	 *
-	 * @param \TYPO3\Jobqueue\Common\Queue\Message $message
-	 * @return boolean TRUE if the message could be removed
-	 */
-	public function finish(\TYPO3\Jobqueue\Common\Queue\Message $message) {
-		$originalValue = $message->getOriginalValue();
-		$success = $this->client->lrem("queue:{$this->name}:processing", 0, $originalValue) > 0;
-		if ($success) {
-			$message->setState(\TYPO3\Jobqueue\Common\Queue\Message::STATE_DONE);
-		}
-		return $success;
-	}
+    /**
+     * Mark a message as finished
+     *
+     * @param Message $message
+     * @return boolean TRUE if the message could be removed
+     */
+    public function finish(Message $message)
+    {
+        $originalValue = $message->getOriginalValue();
+        $success = $this->client->lrem("queue:{$this->name}:processing", 0, $originalValue) > 0;
+        if ($success) {
+            $message->setState(Message::STATE_DONE);
+        }
+        return $success;
+    }
 
-	/**
-	 * Peek for messages
-	 *
-	 * @param integer $limit
-	 * @return array Messages or empty array if no messages were present
-	 */
-	public function peek($limit = 1) {
-		$result = $this->client->lrange("queue:{$this->name}:messages", -($limit), -1);
-		if (is_array($result) && count($result) > 0) {
-			$messages = array();
-			foreach ($result as $value) {
-				$message = $this->decodeMessage($value);
-					// The message is still published and should not be processed!
-				$message->setState(\TYPO3\Jobqueue\Common\Queue\Message::STATE_PUBLISHED);
-				$messages[] = $message;
-			}
-			return $messages;
-		}
-		return array();
-	}
+    /**
+     * Peek for messages
+     *
+     * @param integer $limit
+     * @return Message[] Messages or empty array if no messages were present
+     */
+    public function peek($limit = 1)
+    {
+        $result = $this->client->lrange("queue:{$this->name}:messages", -($limit), -1);
+        if (is_array($result) && count($result) > 0) {
+            $messages = array();
+            foreach ($result as $value) {
+                $message = $this->decodeMessage($value);
+                // The message is still submitted and should not be processed!
+                $message->setState(Message::STATE_SUBMITTED);
+                $messages[] = $message;
+            }
+            return $messages;
+        }
+        return array();
+    }
 
-	/**
-	 * Count messages in the queue
-	 *
-	 * @return integer
-	 */
-	public function count() {
-		$count = $this->client->llen("queue:{$this->name}:messages");
-		return $count;
-	}
+    /**
+     * Count messages in the queue
+     *
+     * @return integer
+     */
+    public function count()
+    {
+        $count = $this->client->llen("queue:{$this->name}:messages");
+        return $count;
+    }
 
-	/**
-	 * Encode a message
-	 *
-	 * Updates the original value property of the message to resemble the
-	 * encoded representation.
-	 *
-	 * @param \TYPO3\Jobqueue\Common\Queue\Message $message
-	 * @return string
-	 */
-	protected function encodeMessage(\TYPO3\Jobqueue\Common\Queue\Message $message) {
-		$value = json_encode($message->toArray());
-		$message->setOriginalValue($value);
-		return $value;
-	}
+    /**
+     * Encode a message
+     *
+     * Updates the original value property of the message to resemble the
+     * encoded representation.
+     *
+     * @param Message $message
+     * @return string
+     */
+    protected function encodeMessage(Message $message)
+    {
+        $value = json_encode($message->toArray());
+        $message->setOriginalValue($value);
+        return $value;
+    }
 
-	/**
-	 * Decode a message from a string representation
-	 *
-	 * @param string $value
-	 * @return \TYPO3\Jobqueue\Common\Queue\Message
-	 */
-	protected function decodeMessage($value) {
-		$decodedMessage = json_decode($value, TRUE);
-		$message = new \TYPO3\Jobqueue\Common\Queue\Message($decodedMessage['payload']);
-		if (isset($decodedMessage['identifier'])) {
-			$message->setIdentifier($decodedMessage['identifier']);
-		}
-		$message->setOriginalValue($value);
-		return $message;
-	}
+    /**
+     * Decode a message from a string representation
+     *
+     * @param string $value
+     * @return Message
+     */
+    protected function decodeMessage($value)
+    {
+        $decodedMessage = json_decode($value, TRUE);
+        $message = new Message($decodedMessage['payload']);
+        if (isset($decodedMessage['identifier'])) {
+            $message->setIdentifier($decodedMessage['identifier']);
+        }
+        $message->setOriginalValue($value);
+        return $message;
+    }
 
-	/**
-	 *
-	 * @param string $identifier
-	 * @return \TYPO3\Jobqueue\Common\Queue\Message
-	 */
-	public function getMessage($identifier) {
-		return NULL;
-	}
+    /**
+     *
+     * @param string $identifier
+     * @return Message
+     */
+    public function getMessage($identifier)
+    {
+        return NULL;
+    }
 
 
 }
-?>
--- Tests/Functional/Queue/RedisQueueTest.php
+++ Tests/Functional/Queue/RedisQueueTest.php
@@ -1,127 +1,140 @@
 <?php
-namespace TYPO3\Jobqueue\Redis\Tests\Functional\Queue;
+namespace Flowpack\JobQueue\Redis\Tests\Functional\Queue;
 
-/*                                                                        *
- * This script belongs to the FLOW3 package "Jobqueue.Redis".                *
- *                                                                        *
- * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU General Public License, either version 3 of the   *
- * License, or (at your option) any later version.                        *
- *                                                                        *
- * The TYPO3 project - inspiring people to share!                         *
- *                                                                        */
+/*
+ * This file is part of the Flowpack.JobQueue.Redis package.
+ *
+ * (c) Contributors to the package
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Flowpack\JobQueue\Common\Queue\Message;
+use Flowpack\JobQueue\Redis\Queue\RedisQueue;
+use Predis\Client as PredisClient;
+use TYPO3\Flow\Configuration\ConfigurationManager;
+use TYPO3\Flow\Tests\FunctionalTestCase;
 
 /**
  * Functional test for RedisQueue
  */
-class RedisQueueTest extends \TYPO3\FLOW3\Tests\FunctionalTestCase {
+class RedisQueueTest extends FunctionalTestCase
+{
 
-	/**
-	 * @var \TYPO3\Jobqueue\Redis\Queue\RedisQueue
-	 */
-	protected $queue;
+    /**
+     * @var RedisQueue
+     */
+    protected $queue;
 
-	/**
-	 * Set up dependencies
-	 */
-	public function setUp() {
-		parent::setUp();
-		$configurationManager = $this->objectManager->get('TYPO3\FLOW3\Configuration\ConfigurationManager');
-		$settings = $configurationManager->getConfiguration(\TYPO3\FLOW3\Configuration\ConfigurationManager::CONFIGURATION_TYPE_SETTINGS, 'TYPO3.Jobqueue.Redis');
-		if (!isset($settings['testing']['enabled']) || $settings['testing']['enabled'] !== TRUE) {
-			$this->markTestSkipped('Test database is not configured');
-		}
+    /**
+     * Set up dependencies
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $configurationManager = $this->objectManager->get(ConfigurationManager::class);
+        $settings = $configurationManager->getConfiguration(ConfigurationManager::CONFIGURATION_TYPE_SETTINGS, 'Flowpack.JobQueue.Redis');
+        if (!isset($settings['testing']['enabled']) || $settings['testing']['enabled'] !== TRUE) {
+            $this->markTestSkipped('Test database is not configured');
+        }
 
-		$this->queue = new \TYPO3\Jobqueue\Redis\Queue\RedisQueue('Test queue', $settings['testing']);
+        $this->queue = new RedisQueue('Test queue', $settings['testing']);
 
-		$client = new \Predis\Client($settings['testing']['client']);
-		$client->flushdb();
-	}
+        $client = new PredisClient($settings['testing']['client']);
+        $client->flushdb();
+    }
 
-	/**
-	 * @test
-	 */
-	public function publishAndWaitWithMessageWorks() {
-		$message = new \TYPO3\Jobqueue\Common\Queue\Message('Yeah, tell someone it works!');
-		$this->queue->publish($message);
+    /**
+     * @test
+     */
+    public function submitAndWaitWithMessageWorks()
+    {
+        $message = new Message('Yeah, tell someone it works!');
+        $this->queue->submit($message);
 
-		$result = $this->queue->waitAndTake(1);
-		$this->assertNotNull($result, 'wait should receive message');
-		$this->assertEquals($message->getPayload(), $result->getPayload(), 'message should have payload as before');
-	}
+        $result = $this->queue->waitAndTake(1);
+        $this->assertNotNull($result, 'wait should receive message');
+        $this->assertEquals($message->getPayload(), $result->getPayload(), 'message should have payload as before');
+    }
 
-	/**
-	 * @test
-	 */
-	public function waitForMessageTimesOut() {
-		$result = $this->queue->waitAndTake(1);
-		$this->assertNull($result, 'wait should return NULL after timeout');
-	}
+    /**
+     * @test
+     */
+    public function waitForMessageTimesOut()
+    {
+        $result = $this->queue->waitAndTake(1);
+        $this->assertNull($result, 'wait should return NULL after timeout');
+    }
 
-	/**
-	 * @test
-	 */
-	public function identifierMakesMessagesUnique() {
-		$message = new \TYPO3\Jobqueue\Common\Queue\Message('Yeah, tell someone it works!', 'test.message');
-		$identicalMessage = new \TYPO3\Jobqueue\Common\Queue\Message('Yeah, tell someone it works!', 'test.message');
-		$this->queue->publish($message);
-		$this->queue->publish($identicalMessage);
+    /**
+     * @test
+     */
+    public function identifierMakesMessagesUnique()
+    {
+        $message = new Message('Yeah, tell someone it works!', 'test.message');
+        $identicalMessage = new Message('Yeah, tell someone it works!', 'test.message');
+        $this->queue->submit($message);
+        $this->queue->submit($identicalMessage);
 
-		$this->assertEquals(\TYPO3\Jobqueue\Common\Queue\Message::STATE_NEW, $identicalMessage->getState());
+        $this->assertEquals(Message::STATE_NEW, $identicalMessage->getState());
 
-		$result = $this->queue->waitAndTake(1);
-		$this->assertNotNull($result, 'wait should receive message');
+        $result = $this->queue->waitAndTake(1);
+        $this->assertNotNull($result, 'wait should receive message');
 
-		$result = $this->queue->waitAndTake(1);
-		$this->assertNull($result, 'message should not be queued twice');
-	}
+        $result = $this->queue->waitAndTake(1);
+        $this->assertNull($result, 'message should not be queued twice');
+    }
 
-	/**
-	 * @test
-	 */
-	public function peekReturnsNextMessagesIfQueueHasMessages() {
-		$message = new \TYPO3\Jobqueue\Common\Queue\Message('First message');
-		$this->queue->publish($message);
-		$message = new \TYPO3\Jobqueue\Common\Queue\Message('Another message');
-		$this->queue->publish($message);
+    /**
+     * @test
+     */
+    public function peekReturnsNextMessagesIfQueueHasMessages()
+    {
+        $message = new Message('First message');
+        $this->queue->submit($message);
+        $message = new Message('Another message');
+        $this->queue->submit($message);
 
-		$results = $this->queue->peek(1);
-		$this->assertEquals(1, count($results), 'peek should return a message');
-		$result = $results[0];
-		$this->assertEquals('First message', $result->getPayload());
-		$this->assertEquals(\TYPO3\Jobqueue\Common\Queue\Message::STATE_PUBLISHED, $result->getState());
+        $results = $this->queue->peek(1);
+        $this->assertEquals(1, count($results), 'peek should return a message');
+        $result = $results[0];
+        $this->assertEquals('First message', $result->getPayload());
+        $this->assertEquals(Message::STATE_SUBMITTED, $result->getState());
 
-		$results = $this->queue->peek(1);
-		$this->assertEquals(1, count($results), 'peek should return a message again');
-		$result = $results[0];
-		$this->assertEquals('First message', $result->getPayload(), 'second peek should return the same message again');
-	}
+        $results = $this->queue->peek(1);
+        $this->assertEquals(1, count($results), 'peek should return a message again');
+        $result = $results[0];
+        $this->assertEquals('First message', $result->getPayload(), 'second peek should return the same message again');
+    }
 
-	/**
-	 * @test
-	 */
-	public function peekReturnsNullIfQueueHasNoMessage() {
-		$result = $this->queue->peek();
-		$this->assertEquals(array(), $result, 'peek should not return a message');
-	}
+    /**
+     * @test
+     */
+    public function peekReturnsNullIfQueueHasNoMessage()
+    {
+        $result = $this->queue->peek();
+        $this->assertEquals(array(), $result, 'peek should not return a message');
+    }
 
-	/**
-	 * @test
-	 */
-	public function waitAndReserveWithFinishRemovesMessage() {
-		$message = new \TYPO3\Jobqueue\Common\Queue\Message('First message');
-		$this->queue->publish($message);
+    /**
+     * @test
+     */
+    public function waitAndReserveWithFinishRemovesMessage()
+    {
+        $message = new Message('First message');
+        $this->queue->submit($message);
 
-		$result = $this->queue->waitAndReserve(1);
-		$this->assertNotNull($result, 'waitAndReserve should receive message');
-		$this->assertEquals($message->getPayload(), $result->getPayload(), 'message should have payload as before');
+        $result = $this->queue->waitAndReserve(1);
+        $this->assertNotNull($result, 'waitAndReserve should receive message');
+        $this->assertEquals($message->getPayload(), $result->getPayload(), 'message should have payload as before');
 
-		$result = $this->queue->peek();
-		$this->assertEquals(array(), $result, 'no message should be present in queue');
+        $result = $this->queue->peek();
+        $this->assertEquals(array(), $result, 'no message should be present in queue');
 
-		$finishResult = $this->queue->finish($message);
-		$this->assertTrue($finishResult);
-	}
+        $finishResult = $this->queue->finish($message);
+        $this->assertTrue($finishResult);
+    }
 
 }
-?>
